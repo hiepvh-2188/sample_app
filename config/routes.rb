7b37b7c60acbff58c941b11d77d869cf3270e00e
hiepvh-2188 <vo.hoang.hiep@sun-asterisk.com>
2021-01-05 14:13:31 +0700

    Readd

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/new'
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users
+end
